0941d9c2d1558616e7b3bdf54e713fc273d90dd6
adeshsharma3 <adeshatnet@gmail.com>
2022-03-30 06:40:31 +0530

    upload

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "Resource_group_name" {
+    type = string
+  description = "Resource Group name in Azure"
+}
+
+variable "Resource_group_location" {
+  
+  type = string
+  description = "Resource Group location in Azure"
+}
+
+variable "App_service_plan" {
+  
+  type = string
+  description = "App service plan in Azure"
+}
+
+variable "App_service_name" {
+    type = string
+  description = "App service name in Azure"
+}
